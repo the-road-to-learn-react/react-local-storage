c15e8a0a13ddf7db03188b7e59c5f21be1858075
Robin Wieruch <wrobin@gmx.net>
2017-04-20 16:20:21 +0800

    finish

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { hits: null };
+  }
+
+  onSearch = (e) => {
+    e.preventDefault();
+
+    const { value } = this.input;
+
+    if (value === '') {
+      return;
+    }
+
+    const cachedHits = localStorage.getItem(value);
+    if (cachedHits) {
+      this.setState({ hits: JSON.parse(cachedHits) });
+      return;
+    }
+
+    fetch('https://hn.algolia.com/api/v1/search?query=' + value)
+      .then(response => response.json())
+      .then(result => this.onSetResult(result, value));
+  }
+
+  onSetResult = (result, key) => {
+    localStorage.setItem(key, JSON.stringify(result.hits));
+    this.setState({ hits: result.hits });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Search Hacker News with Local Storage</h1>
+        <p>There shouldn't be a second network request, when you search for something twice.</p>
+
+        <form type="submit" onSubmit={this.onSearch}>
+          <input type="text" ref={node => this.input = node} />
+          <button type="text">Search</button>
+        </form>
+
+        {
+          this.state.hits &&
+          this.state.hits.map(item => <div key={item.objectID}>{item.title}</div>)
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
