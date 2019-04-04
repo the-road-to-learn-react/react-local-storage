eb2e5cc7cb4f264ab9aab7dd6b2c9c3db04acc4f
Robin Wieruch <wrobin@gmx.net>
2019-04-04 15:55:05 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+const useStateWithLocalStorage = localStorageKey => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(localStorageKey) || ''
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(localStorageKey, value);
+  }, [value]);
+
+  return [value, setValue];
+};
 
 const App = () => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = useStateWithLocalStorage(
+    'myValueInLocalStorage'
+  );
 
   const onChange = event => setValue(event.target.value);
 
@@ -17,35 +31,3 @@ const App = () => {
 };
 
 export default App;
-
-// const useStateWithLocalStorage = localStorageKey => {
-//   const [value, setValue] = React.useState(
-//     localStorage.getItem(localStorageKey) || ''
-//   );
-
-//   React.useEffect(() => {
-//     localStorage.setItem(localStorageKey, value);
-//   }, [value]);
-
-//   return [value, setValue];
-// };
-
-// const App = () => {
-//   const [value, setValue] = useStateWithLocalStorage(
-//     'myValueInLocalStorage'
-//   );
-
-//   const onChange = event => setValue(event.target.value);
-
-//   return (
-//     <div>
-//       <h1>Hello React Function Component!</h1>
-
-//       <input value={value} type="text" onChange={onChange} />
-
-//       <p>{value}</p>
-//     </div>
-//   );
-// };
-
-// export default App;
